c369bf33f2d6436c2fa5a71acaef762d549e86e1
Tomasz Sikora <frakti7@gmail.com>
2015-11-13 13:42:10 +0100

    Voucher redemption

--- java-app/src/main/java/shop/Main.java
+++ java-app/src/main/java/shop/Main.java
@@ -1,34 +1,45 @@
 package shop;
 
 import pl.rspective.voucherify.client.VoucherifyClient;
+import pl.rspective.voucherify.client.VoucherifyUtils;
 import pl.rspective.voucherify.client.callback.VoucherifyCallback;
+import pl.rspective.voucherify.client.exception.VoucherifyException;
 import pl.rspective.voucherify.client.model.Voucher;
 import retrofit.RetrofitError;
-import rx.Subscriber;
-import rx.schedulers.Schedulers;
+
+import java.math.BigDecimal;
 
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     VoucherifyClient client = new VoucherifyClient.Builder()
             .setAppId("c70a6f00-cf91-4756-9df5-47628850002b")
             .setAppToken("3266b9f8-e246-4f79-bdf0-833929b1380c")
+//            .setLogLevel(RestAdapter.LogLevel.FULL)
             .build();
 
     System.out.println("Talking with voucherify.io");
 
+    final String TEST_CODE = "Testing7fjWdr";
 
-    try {
-      Voucher voucher = client.vouchers().fetchVoucher("Testing7fjWdr");
-      System.out.println("Sync result: " + voucher.getDiscount());
+    client.vouchers().async().fetchVoucher(TEST_CODE, new VoucherifyCallback<Voucher>() {
 
-    } catch (RetrofitError e) {
-      System.out.println("Error " + e.getBody().toString());
-    }
-
-    client.vouchers().async().fetchVoucher("Testing7fjWdr", new VoucherifyCallback<Voucher>() {
       @Override
-      public void onSuccess(Voucher result) {
-        System.out.println(result.toString());
+      public void onSuccess(Voucher voucher) {
+        BigDecimal originalPrice = BigDecimal.valueOf(8);
+        BigDecimal newPrice = VoucherifyUtils.calculatePrice(originalPrice, voucher);
+        BigDecimal discount = VoucherifyUtils.calculateDiscount(originalPrice, voucher);
+
+        System.out.println("Original price: " + originalPrice.toString());
+        System.out.println("Price after discount: " + newPrice.toString());
+        System.out.println("Discount: " + discount.toString());
+        System.out.println("Discount Type: " + voucher.getDiscountType());
+
+
+        Voucher voucherAfterRedemption = client.vouchers().redeem(TEST_CODE, "static-main-java-by-frakti");
+
+
+        int redeemedQuantity = voucherAfterRedemption.getRedemption().getRedeemedQuantity();
+        System.out.println("Redeemed quantity: " + redeemedQuantity);
       }
 
       @Override
@@ -37,27 +48,9 @@ public class Main {
       }
     });
 
-    client.vouchers()
-            .rx()
-            .fetchVoucher("VOUCHER_CODE")
-            .subscribeOn(Schedulers.io())
-            .subscribe(new Subscriber<Voucher>() {
 
-              @Override
-              public void onCompleted() {
-                System.out.println("Rx completed");
-              }
 
-              @Override
-              public void onError(Throwable e) {
-                System.out.println("error e" + e.toString());
-              }
 
-              @Override
-              public void onNext(Voucher voucher) {
-
-                System.out.println("RX: " + voucher.getDiscount());
-              }
-            });
+    Thread.sleep(2000);
   }
 }
